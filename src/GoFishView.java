336b4d075b1d43a1f206631dfafd04e76935d71c
Ashneet <96892013+ashneet1@users.noreply.github.com>
2022-08-05 00:25:44 -0400

    Update GoFishView.java

--- src/GoFishView.java
+++ src/GoFishView.java
@@ -2,43 +2,71 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GoFishView extends Pane {
-    private int x = 10;
-    private int y = 10;
-    private int i = 0;
+    boolean containsX;
+    boolean containsY;
+    private int b = 20;
+    private int y=0;
+    private int c = 0;
     private Button[] theCards;
-    private Label points, chances;
+    private Label points, chances, pointsLb, chancesLb;
+    public ArrayList<Integer> xCoordinates;
+    public ArrayList<Integer> yCoordinates;
     public GoFishView(GoFish model){
+        containsX = true;
+        containsY = true;
+        Random rand = new Random();
+        xCoordinates = new ArrayList<Integer>();
+        for(int j = 0; j<3;j++) {
+            for (int i = 0; i < 480; i = i + 70) {
+                xCoordinates.add(i);
+            }
+        }
+        yCoordinates = new ArrayList<Integer>();
+
+        for(int k = 0; k<190;k+=60) {
+            for (int l = 0; l < 7; l++) {
+                yCoordinates.add(k);
+            }
+        }
+
+        pointsLb = new Label("Points");
+        pointsLb.relocate(25,325);
         points = new Label(String.valueOf(model.getPlayer().get_points()));
         points.relocate(30,350);
         points.setPrefSize(200,25);
 
+        chancesLb = new Label("Chances");
+        chancesLb.relocate(85,325);
         chances = new Label(String.valueOf(model.getPlayer().get_chances()));
         chances.relocate(90,350);
         chances.setPrefSize(110,25);
-        getChildren().addAll(points,chances);
-
+        getChildren().addAll(pointsLb,points,chancesLb,chances);
 
         setPrefSize(500,500);
         theCards = new Button[model.getAllCards().size()];
         for(Cards newCard: model.getAllCards()){
+            containsX = true;
+            containsY = true;
             //need to randomize location of cards
-            theCards[i] = new Button();
-            theCards[i].setPrefSize(60,50);
-            theCards[i].relocate(x,y);
-
-            x+=80;
-            getChildren().add(theCards[i]);i+=1;
-            if(x >480){ x = 10;y += 55;}
+            theCards[c] = new Button();
+            theCards[c].setPrefSize(60,50);
+            int rando = rand.nextInt(0,b);
+            theCards[c].relocate(xCoordinates.get(rando),yCoordinates.get(rando));
+            xCoordinates.remove(rando);
+            yCoordinates.remove(rando);
+            b-=1;
+            getChildren().add(theCards[c]);
+            c+=1;
         }
     }
 
-
     public void update(GoFish model) {
-        x = 10;
-        y = 10;
         if(model.getUp1() != -1 & model.getUp2() != -1) {
             if (theCards[model.getUp1()].getText() == theCards[model.getUp2()].getText()) {
                 model.getPlayer().add_points();
